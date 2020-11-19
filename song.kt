f220d087a0d251dfa4cbd1356ece18a1256bddb4
EngMakenzi <stevomakenzi@gmail.com>
2020-11-19 14:54:13 +0300

    Add files via upload
    First Commit: Favorite song attributes

---
+++ song.kt
@@ -0,0 +1,25 @@
+fun main() {
+    //Favorite Song: Just Give Me a Reason
+
+    /*Attributes of my favorite song
+     * Seven main attributes are explored
+     */
+
+    val artist = "Pink"           // This is the main artist
+    val featured = "Nate"         // Pink was assisted by Nate
+    val producer = "Jeff Bhasker" // Jeff is the producer
+    val releaseDate = 2012        // The song was released in the year 2012
+    val genre = "Pop"             // It is a pop song
+    val duration = 4.02           // It lasts for for about 4 minutes
+    val isEnjoyable = true        // Its a good song that one can enjoy
+
+    //Printing out
+    println(artist)
+    println(featured)
+    println(producer)
+    println(releaseDate)
+    println(genre)
+    println(duration)
+    println(isEnjoyable)
+
+}
